f9e7940a78f027f066aac1cb371428717ef446de
Seolhee Kim <seolhee@upspringmedia.com>
2017-08-26 18:29:17 -1000

    added id

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Book = ({ title, author }) => {
+const Book = ({ title, author, id }) => {
   return (
     <li>
       <h3>{ title }</h3>
       <p>{ author }</p>
+      <p>{ id }</p>
     </li>
   )
 }
